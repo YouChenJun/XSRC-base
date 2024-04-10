e6d09e94d9c62b1e433d9d655cda38b4adf9f591
YouChen <kalibug@qq.com>
2024-04-10 22:48:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,11 +100,11 @@ if [ -d "$HOME/osmedeus-base/data" ]; then
     mv $HOME/osmedeus-base $BAK_DIST
 fi
 
-announce "Cloning Osmedeus base repo:\033[0m https://github.com/YouChenJun/XSRC-base"
-rm -rf $BASE_PATH && git clone --quiet --depth=1 https://github.com/YouChenJun/XSRC-base $BASE_PATH
+announce "Cloning Osmedeus base repo:\033[0m https://mirror.ghproxy.com/https://github.com/YouChenJun/XSRC-base"
+rm -rf $BASE_PATH && git clone --quiet --depth=1 https://mirror.ghproxy.com/https://github.com/YouChenJun/XSRC-base $BASE_PATH
 # # retry to clone in case of anything wrong with the connection
 if [ ! -d "$BASE_PATH" ]; then
-    git clone --quiet --depth=1 https://github.com/YouChenJun/XSRC-base $BASE_PATH
+    git clone --quiet --depth=1 https://mirror.ghproxy.com/https://github.com/YouChenJun/XSRC-base $BASE_PATH
 fi
 
 [ -z "$(which osmedeus)" ] && osmBin=/usr/local/bin/osmedeus || osmBin=$(which osmedeus)
